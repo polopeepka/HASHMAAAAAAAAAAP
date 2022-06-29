d154325d5eff1711d543c529d9c78e1ec67d9aba
polopeepka <106977777+polopeepka@users.noreply.github.com>
2022-06-29 17:57:47 +0600

    i wanna be mentor
    HASHMAAAAAP

---
+++ IDK_how_IT.py
@@ -0,0 +1,15 @@
+import random
+
+class Spin:
+    def __init__(self,spin,stop):
+        self.spin = spin,
+        self.stop = stop
+    def start_spinner (self):
+        print(f'Spinner {self.spin} was started')
+    def stop_spinner (self):
+        print(f'Spinner {self.stop} was stopped')
+
+print(Spin)
+
+
+
